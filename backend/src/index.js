7127b2b82709893faec79b933226bec8b4bbaf08
João Pedro Basso Audibert <hotsjoao@gmail.com>
2020-03-28 16:25:05 -0300

    stating sequelize

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
+const routes = require('./routes');
 
 const PORT = 3333;
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.get('/', (req, res) => {
-  return res.json('Hello World');
-})
 
 app.listen(PORT);
